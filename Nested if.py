145e44c6328453840d116d5a8a88e9f8a133a8e5
lakshmikala-17 <84371485+lakshmikala-17@users.noreply.github.com>
2021-05-28 22:33:26 +0530

    Create Nested if.py

---
+++ Nested if.py
@@ -0,0 +1,9 @@
+m=int(input("enter marks "))
+if m>=80 and m<=100:
+  Print ("A grade ")
+elif m>=60 and m<=80:
+  Print ("B grade ")
+elif m>=40 and m<=60:
+  Print ("C grade")
+else:
+  Print ("FAIL")
